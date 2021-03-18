5495b6a56693b413f3b11f1cc5fc849fae639516
= <=>
2021-03-18 14:01:45 +0100

    Updateret random funktionen

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -1,10 +1,14 @@
 (ns memorygame.app
   (:require
     [reagent.core :as r]
-    [reagent.dom :as rdom]))
+    [reagent.dom :as rdom]
+    [memorygame.random :as rrandom ]))
 
 (defn miniapp []
-      [:<>])
+      [:div (str rrandom/generator)   ]
+
+)
+
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
       (rdom/render [miniapp] (js/document.getElementById "app")))
---
+++ src/main/memorygame/random.cljs
@@ -0,0 +1,6 @@
+(ns memorygame.random)
+
+;; https://dilbert.com/strip/2001-10-25
+
+(defn generator []
+      range(16))
