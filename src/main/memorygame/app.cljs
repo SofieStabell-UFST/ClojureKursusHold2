811d7e032f503bf31a074ef5f72e53d4b493aefd
rokhSkat <Rohita.Khatiwada@ufst.dk>
2021-03-18 15:14:42 +0100

    dynamic display of div

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -3,8 +3,19 @@
     [reagent.core :as r]
     [reagent.dom :as rdom]))
 
+(defn display-cards [i]
+      [:div.col
+       [:div.flip-card
+        [:div.flip-card-inner
+         [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
+         [:div.flip-card-back [:img {:src "images/halloween-\\" (js/parseInt i)  ".png"}]]]]]
+      )
+
+
 (defn miniapp []
-      [:<>])
+      [:div (for [i (range 1 16)] (display-cards i))]
+      )
+
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
       (rdom/render [miniapp] (js/document.getElementById "app")))
