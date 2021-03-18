e7746e064f8a0a8c6cfd49c71e296bc6c6720210
Annibeth Strømsten <annibeth.stroemsten@ufst.dk>
2021-03-18 13:59:19 +0100

    Added function that make hiccup code for cards.

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -2,9 +2,27 @@
   (:require
     [reagent.core :as r]
     [reagent.dom :as rdom]))
+(defn card [n]
+      "return hiccup for card n"
+       [:div.flip-card
+       [:div.flip-card-inner
+        [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
+        ;[:div.flip-card-back [:img {:src "images/halloween-8.png"}]]
+        [:div.flip-card-back [:img {:src (str "images/halloween-"n".png")}]]
+        ]]
+
+  )
 
 (defn miniapp []
-      [:<>])
+      [:body
+       [:div.col
+        (for [n (range 1 9)]
+             (card n)
+             )
+        ]
+       ;[:div#app]
+       ;[:script {:src "scripts/core.js"}]
+       ])
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
       (rdom/render [miniapp] (js/document.getElementById "app")))
