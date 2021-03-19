7b123ff5fae74c2056bdda72ef63f5aa489c002e
= <=>
2021-03-19 21:31:32 +0100

    Genererer matrix af kort

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -4,68 +4,63 @@
     [reagent.dom :as rdom]))
 
 (def tilstand (r/atom {
- :næstespiller :player1
- :player1 {
-           :points 0
-           :name "Peter"
-           :antalvundnespil 0
-           }
- :player2 {
-           :points 0
-           :name "Shane"
-           :antalvundnespil 0
-           }
- }))
+                       :næstespiller :player1
+                       :player1      {
+                                      :points          0
+                                      :name            "Peter"
+                                      :antalvundnespil 0
+                                      }
+                       :player2      {
+                                      :points          0
+                                      :name            "Shane"
+                                      :antalvundnespil 0
+                                      }
+                       }))
 
 
 (defn kort [xs]
       [:td {:id (str "felt-" (first xs))}
        [:div.col
-         [:div.flip-card
-          [:div.flip-card-inner
-           [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
-           [:div.flip-card-back [:img {:src (str "images/halloween-" (last xs) ".png")}]]]]]])
+        [:div.flip-card
+         [:div.flip-card-inner
+          [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
+          [:div.flip-card-back [:img {:src (str "images/halloween-" (last xs) ".png")}]]]]]])
 
 
 (defn generer-billedrækken [x] (shuffle (concat (range 1 (inc x)) (range 1 (inc x)))))
 
 
+
 (defn miniapp []
       (let [
             kortrækken (map kort (zipmap (range 1 17) (shuffle (concat (range 1 9) (range 1 9)))))
+            kortmatrix (partition 4 kortrækken)
+            tr (fn [x] [:tr x])
             player1_points 0
             player2_points 0]
 
-     [:div.hele-molevitten
-      [:table {:border 1}
-       [:tr
-        (take 4 kortrækken)]
-       [:tr
-        (take 4 (drop 4 kortrækken))]
-       [:tr
-        (take 4 (drop 8 kortrækken))]
-       [:tr
-        (take 4 (drop 12 kortrækken))]]
-
-       [:div.scoreboard
-        [:table
-         [:tbody
-          [:tr
-            [:td "Ny runde"]
-            [:td [:button "Ny runde!"]]]
-          [:tr
-           [:td "Player 1"]
-           [:td player1_points]]
-          [:tr
-           [:td "Player 2"]
-           [:td player2_points]]
-          [:tr
-           [:td "Næste tur"]
-           [:td (@tilstand :næstespiller ) ]]
-          [:tr
-           [:td "Total Score i samtilige spil"]
-           [:td "Player1 20 Player2 30"]
-           ]]]]]))
+           [:div.hele-molevitten
+            [:table {:border 1}
+             (map tr kortmatrix)]
+            [:div.scoreboard
+             [:table
+              [:tbody
+               [:tr
+                [:td "Ny runde"]
+                [:td [:button "Ny runde!"]]]
+               [:tr
+                [:td "Player 1"]
+                [:td player1_points]]
+               [:tr
+                [:td "Player 2"]
+                [:td player2_points]]
+               [:tr
+                [:td "Næste tur"]
+                [:td (@tilstand :næstespiller)]]
+               [:tr
+                [:td "Total Score i samtilige spil"]
+                [:td "Player1 20 Player2 30"]
+                ]]]]]))
 
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
@@ -73,4 +68,4 @@
 
 (defn ^:export reload []
       (js/console.log "reload...")
-      (run)2)
+      (run) 2)
