db5edeb2dd662ec40e8a0690f5c0b1311b265ba7
Amal Qasim <amal.qasim@ufst.dk>
2021-03-19 15:29:15 +0100

    Added flip functions.

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -2,14 +2,34 @@
   (:require
     [reagent.core :as r]
     [reagent.dom :as rdom]))
+
+; Hold 2 -plan:
+;Amal, Tue og Annibeth
+;
+;memory game
+;
+;1 flip kun ved klik event
+;
+;2   1 vendt not 2 vendt
+;    1 vendt eq 2 vendt = poæng
+
+
+(def cardArray [[true false] [true false]])
+
+
 (defn card [n]
       "return hiccup for card n"
-      [:div.flip-card
-       [:div.flip-card-inner
-        [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
-        ;[:div.flip-card-back [:img {:src "images/halloween-8.png"}]]
-        [:div.flip-card-back [:img {:src (str "images/halloween-" n ".png")}]]
-        ]]
+      (let  [ flip? (r/atom true)]
+           [:div.flip-card
+            [:div.flip-card-inner
+             {
+              :class [(when @flip? "flip")]
+              :on-click (fn [] (swap! flip? not)
+                            (js/console.log @flip?))
+              }
+             [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
+             [:div.flip-card-back [:img {:src (str "images/halloween-" n ".png")}]]
+             ]])
       )
 
 
@@ -33,7 +53,7 @@
                )
           ]
          [:tr
-          (for [n (range 1 9)]
+          (for [n (range 5 9)]
                [:td (card n)]
                )
           ]
@@ -51,3 +71,4 @@
 (defn ^:export reload []
       (js/console.log "reload...")
       (run))
+
