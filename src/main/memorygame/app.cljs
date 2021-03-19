32ace3a1fb343d329aa2f50f4ff5b009dd0ba6e7
Shane Rich <srichnz@gmail.com>
2021-03-19 18:45:55 +0100

    nyt-spil funktion

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -3,7 +3,11 @@
     [reagent.core :as r]
     [reagent.dom :as rdom]))
 
+(def a (r/atom nil))
+(def billedrækken (r/atom (zipmap (range 1 17) (shuffle (concat (range 1 9) (range 1 9))))))
 (def tilstand (r/atom {
+                       :kort nil
+
                        :næstespiller :player1
                        :player1      {
                                       :points          0
@@ -29,11 +33,20 @@
 
 (defn generer-billedrækken [x] (shuffle (concat (range 1 (inc x)) (range 1 (inc x)))))
 
+(defn spil [event]
+      (js/console.log event)
+      (print @a)
+      (reset! a event)
+      (print @a))
+
+(defn nyt-spil []
+      (reset! billedrækken (zipmap (range 1 17) (shuffle (concat (range 1 9) (range 1 9))))))
 
 
 (defn miniapp []
       (let [
             kortrækken (map kort (zipmap (range 1 17) (shuffle (concat (range 1 9) (range 1 9)))))
+            ;; funktionen (fn [xs] [:td {:on-click #(spil "Jeg fungerer") :id (str "felt-" (first xs))} (tegn-felt (last xs))])
             kortmatrix (partition 4 kortrækken)
             tr (fn [x] [:tr x])
             player1_points 0
@@ -47,7 +60,7 @@
               [:tbody
                [:tr
                 [:td "Ny runde"]
-                [:td [:button "Ny runde!"]]]
+                [:td [:input {:type "button" :value "Start ny runde" :on-click #(nyt-spil)}]]
                [:tr
                 [:td "Player 1"]
                 [:td player1_points]]
@@ -61,6 +74,7 @@
                 [:td "Total Score i samtilige spil"]
                 [:td "Player1 20 Player2 30"]
                 ]]]]]))
+
 
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
