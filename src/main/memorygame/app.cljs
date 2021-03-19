ffbe33d490fb7e1570b5a00eb254f6b2865c2b4d
rokhSkat <Rohita.Khatiwada@ufst.dk>
2021-03-19 08:00:07 +0100

    display panel added

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -8,13 +8,32 @@
        [:div.flip-card
         [:div.flip-card-inner
          [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
-         [:div.flip-card-back [:img {:src (str "images/halloween-"i".png")}]]]]]
+         [:div.flip-card-back [:img {:src (str "images/halloween-" i ".png")}]]]]]
+      )
+
+(defn display-control-panel []
+      [:div.display-panel-container
+       [:h1.display-panel-header "Memory Game"]
+       [:div.dp-body
+        [:div.dp-section1
+         [:p.dp-label "Player 1:  " "Name"
+          [:p.dp-label "Player 2:  " "Name"
+           [:p.dp-label "Round: " " Value"]]]]
+        [:div.dp-section2
+         [:p.dp-label "Players turn:  " "Name"
+           [:p.dp-label "Score: " " Value"]]]
+        [:div.dp-section1
+         [:div.dp-btn-container
+          [:button.dp-btn "Start"]
+          [:button.dp-btn "Restart"]]
+         [:p.dp-winner.item "*********The winner of this game is********"]
+         ]]]
       )
 
 
 (defn miniapp []
-      [:div (for [i (range 1 16)] (display-cards i))]
-      )
+      [:div (display-control-panel)
+       [:div.flex-container (for [i (range 16)] (display-cards i))]])
 
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
