3019842fe7a1b9138f095f81783ab6c23187167a
Shane Rich <srichnz@gmail.com>
2021-03-22 16:03:43 +0100

    spil-funktionen udbygget

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -4,41 +4,42 @@
     [reagent.dom :as rdom]))
 
 (def a (r/atom nil))
-(def billedrækken (r/atom (zipmap (range 1 17) (shuffle (concat (range 1 9) (range 1 9))))))
-(def tilstand (r/atom {
-                       :kort nil
+(def billedrækken-2 (r/atom (zipmap (range 1 17) (shuffle (concat (range 1 9) (range 1 9))))))
+(def billedrækken (r/atom (zipmap (range 1 17) (concat (range 1 9) (range 1 9)))))
+(def tilstand (atom {
+                     :kort         nil
 
-                       :næstespiller :player1
-                       :player1      {
-                                      :points          0
-                                      :name            "Peter"
-                                      :antalvundnespil 0
-                                      }
-                       :player2      {
-                                      :points          0
-                                      :name            "Shane"
-                                      :antalvundnespil 0
-                                      }
-                       }))
+                     :næstespiller :player1
+                     :player1      {
+                                    :points          0
+                                    :name            "Peter"
+                                    :antalvundnespil 0
+                                    }
+                     :player2      {
+                                    :points          0
+                                    :name            "Shane"
+                                    :antalvundnespil 0
+                                    }
+                     }))
 
 (defn spil [event]
-      (js/console.log event)
-      (print @a)
-      (reset! a event)
-      (print @a))
+      (let [[placering kort] event]
+           (if (nil? (@tilstand :kort))
+             (do
+               (swap! tilstand assoc-in [:kort] kort))
+             (do
+               (when (= kort (@tilstand :kort)) (swap! tilstand update-in [:player1 :points] inc)))))
+      (print @tilstand))
 
 (defn kort [xs]
-      [:td {:on-click #(spil "Jeg fungerer") :id (str "felt-" (first xs))}
+      [:td {:on-click #(spil xs) :id (str "felt-" (first xs))}
        [:div.col
         [:div.flip-card
          [:div.flip-card-inner
           [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
           [:div.flip-card-back [:img {:src (str "images/halloween-" (last xs) ".png")}]]]]]])
 
-;; funktionen (fn [xs] [:td {:on-click #(spil "Jeg fungerer") :id (str "felt-" (first xs))} (tegn-felt (last xs))])
-
-
-(defn generer-billedrækken [x] (shuffle (concat (range 1 (inc x)) (range 1 (inc x)))))
+;; (defn generer-billedrækken [x] (shuffle (concat (range 1 (inc x)) (range 1 (inc x)))))
 
 
 (defn nyt-spil []
@@ -47,7 +48,7 @@
 
 (defn miniapp []
       (let [
-            kortrækken (map kort billedrækken)
+            kortrækken (map kort @billedrækken)
             kortmatrix (partition 4 kortrækken)
             tr (fn [x] [:tr x])
             player1_points 0
@@ -62,19 +63,19 @@
                [:tr
                 [:td "Ny runde"]
                 [:td [:input {:type "button" :value "Start ny runde" :on-click #(nyt-spil)}]]
-               [:tr
-                [:td "Player 1"]
-                [:td player1_points]]
-               [:tr
-                [:td "Player 2"]
-                [:td player2_points]]
-               [:tr
-                [:td "Næste tur"]
-                [:td (@tilstand :næstespiller)]]
-               [:tr
-                [:td "Total Score i samtilige spil"]
-                [:td "Player1 20 Player2 30"]
-                ]]]]]))
+                [:tr
+                 [:td "Player 1"]
+                 [:td player1_points]]
+                [:tr
+                 [:td "Player 2"]
+                 [:td player2_points]]
+                [:tr
+                 [:td "Næste tur"]
+                 [:td (@tilstand :næstespiller)]]
+                [:tr
+                 [:td "Total Score i samtilige spil"]
+                 [:td "Player1 20 Player2 30"]
+                 ]]]]]]))
 
 
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
