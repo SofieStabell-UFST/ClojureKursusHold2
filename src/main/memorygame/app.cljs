1202e1089e47fa99c412ddcc28c7454a5d0ba9ef
Mathias H. Hansen <mathias.hansen@ufst.dk>
2021-03-23 12:22:15 +0100

    cards shuffled

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -18,13 +18,33 @@
       )
 
 (defn toggle-card [i]
+  (comment
       (if @flip
         (show-card i)
         (hide-card)
-        )
-      )
+        )))
 
 (defn display-cards [n]
+      (js/console.log "n:")
+      (js/console.log n)
+
+      [:div.col
+       [:div.flip-card
+        [:div.flip-card-inner
+         [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
+         [:div.flip-card-back [:img {:src "images/halloween-1.png"}]]]]]
+         ;; [:div.flip-card-back [:img {:src "images/halloween-\\" (js/parseInt i)  ".png"}]]]]]
+
+;; [:div.flip-card
+            ;; [:div.flip-card-inner
+             ;; {:class    ["flip"]
+              ;; :on-click (js/console.log "flip value inside show")
+              ;; }
+             ;; [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
+             ;; [:div.flip-card-back [:img {:src (str "images/halloween-" n ".png")}]]
+             ;; ]
+            ;; ]
+      (comment
       (let [flip? (r/atom false)]
            [:div.flip-card
             [:div.flip-card-inner
@@ -46,7 +66,7 @@
              [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
              [:div.flip-card-back [:img {:src (str "images/halloween-" n ".png")}]]
              ]
-            ]))
+            ])))
 
 
 
@@ -72,24 +92,24 @@
       )
 
 
-;; TODO - work in progress
-;; Creates 4 rows and 4 columns with random number
-;; But we still needs to make sure a number is only used two times
-;; (for [_ (range 1 5)]
-;;  (do
-;;    (for [i (range 1 5)]
-;;      (let [randno (rand-nth (range 1 9))]
-;;        (str randno)))))
+(def data 
+  "create 16 maps with 8 maps/pairs from 1-8 with a boolean false. 
+  false is going to be used to check if these cards is done" 
+  (shuffle [{1 false} {2 false} {3 false} {4 false}
+            {1 false} {2 false} {3 false} {4 false}
+            {5 false} {6 false} {7 false} {8 false}
+            {5 false} {6 false} {7 false} {8 false}
+            ]))
+;; (for [row data] (println "row: " row))
+;; (print data)
+;; (shuffle data)
 
 (defn miniapp []
       [:div (display-control-panel)
-       ;;rand-nth (range 1 9))
        [:div.flex-container
-        (for [_ (range 1 3)]
-             (for [i (range 1 3)] (display-cards i))
-             )]
-       ]
-      )
+        ;; (for [_ (range 1 3)]
+             ;; (for [i (range 1 3)] (display-cards i))
+             (for [x data] (display-cards x))]])
 
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
