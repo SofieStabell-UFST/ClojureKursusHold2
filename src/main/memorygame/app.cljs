f84811d21aaa5836b542d087549a8f9681613e4b
Annibeth Strømsten <annibeth.stroemsten@ufst.dk>
2021-03-24 15:59:44 +0100

    changed strategi

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -2,53 +2,50 @@
   (:require
     [reagent.core :as r]
     [reagent.dom :as rdom]))
-
-(def card-clicked (r/atom 0))
-(def y (r/atom false))
-(def card-store [[(r/atom false) (r/atom false)] [(r/atom false) (r/atom false)]])
-
+(def card-clicked (r/atom nil))
+(defonce card-store (r/atom [[false false] [false false]]))
+(defn deck-first [[a b]]
+      [(dec a) b])
 (defn card [n i]
       "return hiccup for card n"
-
-          [:div.flip-card
-           [:div.flip-card-inner
-            {:class    [(when @(get-in card-store [n i]) "flip")]
-             :on-click (fn []
-                           (do (if (or (= n @card-clicked) (= 0 @card-clicked))
-                                 (do (swap! (get-in card-store [n i]) not)
-                                     (reset! card-clicked n))
-                                 ;else
-                                 (do (reset! card-clicked 0)
-                                     (reset! (get-in card-store [n (- 1 i)]) false)))))
-             }
-            [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
-            [:div.flip-card-back [:img {:src (str "images/halloween-" n ".png")}]]
-            ]
-           ])
-
-(def x (r/atom false))
-(def z (r/atom false))
-(def a (r/atom false))
+      [:div.flip-card
+       [:div.flip-card-inner
+        {:class    [(when (get-in @card-store [(dec n) i]) "flip")]
+         :on-click (fn []
+                       (when (not (get-in @card-store [(dec n) i]))
+                             (cond (= nil @card-clicked) (do (swap! card-store (fn [card-store] (assoc-in card-store [(dec n) i] true)))
+                                                             (reset! card-clicked [n i]))
+                                   (= n (first @card-clicked)) (do (swap! card-store (fn [card-store] (assoc-in card-store [(dec n) i] true)))
+                                                                   (reset! card-clicked nil))
+                                   :else (do (swap! card-store (fn [card-store] (assoc-in card-store [(dec n) i] true)))
+                                             (js/setTimeout
+                                               (let [x @card-clicked]
+                                                    (fn []
+                                                        (swap! card-store (fn [card-store] (assoc-in card-store [(dec n) i] false)))
+                                                        (swap! card-store (fn [card-store] (assoc-in card-store (deck-first x) false)))))
+                                               1000)
+                                             (reset! card-clicked nil)
+                                             ))))}
+        [:div.flip-card-front [:img {:src "images/halloween-background.png"}]]
+        [:div.flip-card-back [:img {:src (str "images/halloween-" n ".png")}]]
+        ]
+       ])
 
 (defn miniapp []
-      [:body
-       [:table
-        [:tbody
-         [:tr
-          [:td (card 1 0)]
-          [:td (card 1 1)]
-          ]
-         [:tr
-          [:td (card 2 0)]
-          [:td (card 2 1)]
-          ]
-         ]]
-       [:div.col
-
-        ]
-       ;[:div#app]
-       ;[:script {:src "scripts/core.js"}]
-       ])
+      [:table
+       [:tbody
+        [:tr
+         [:td [card 1 0]]
+         [:td [card 1 1]]
+         ]
+        [:tr
+         [:td [card 2 0]]
+         [:td [card 2 1]]
+         ]
+        ]]
+      ;[:div#app]
+      ;[:script {:src "scripts/core.js"}]
+      )
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
       (rdom/render [miniapp] (js/document.getElementById "app")))
