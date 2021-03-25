a8b6077eb66716d70274c77ea068d2c1e58a1b57
rich-s-d <64591348+rich-s-d@users.noreply.github.com>
2021-03-25 08:47:08 +0100

    refactored the scoreboard and nyt spil funktion for you (#10)
    * refactored the scoreboard and nyt spil funktion for you
    
    * two missing brackets added to spil function so the program builds

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -61,7 +61,7 @@
                (css/remove-class! (-> js/document (.getElementById (@tilstand :placering))) "flip-card-transform")
                (swap! tilstand update-in [:næstespiller] inc)
                (swap! tilstand assoc-in [:placering] nil)
-               (swap! tilstand assoc-in [:kort] nil)))
+               (swap! tilstand assoc-in [:kort] nil)))))
            ; tjek om spillet er slut.
 
 
@@ -86,9 +86,7 @@
 (defn nyt-spil []
       (reset!
         billedrækken
-        (zipmap (range 1 17)
-                (generer-billedrækken 8)
-                )))
+        (zipmap (range 1 17) (generer-billedrækken 8))))
 
 
 (defn miniapp []
@@ -109,18 +107,21 @@
                 [:td "Ny runde"]
                 [:td [:input {:type "button" :value "Start ny runde" :on-click #(nyt-spil)}]]]
                [:tr
-                [:td "Player 1"]
-                [:td {:colspan "1"} [:input#result {:readonly "" :type "text" :value ((@tilstand :player1) :points)}]]]
+                [:td "Player 1: point i nuværende runde ="]
+                ;[:td  {:colspan "1"} [:input#result {:readonly "" :type "text" :value  ((@tilstand :player1) :points) }]]
+                [:td ((@tilstand :player1) :points)]]
                [:tr
-                [:td "Player 2"]
+                [:td "Player 2: point i nuværende runde ="]
                 [:td ((@tilstand :player2) :points)]]
                [:tr
-                [:td "Næste tur"]
+                [:td "Næste tur:"]
                 [:td (@tilstand :næstespiller)]]
                [:tr
-                [:td "Total Score i samtilige spil"]
-                [:td "Player1 20 Player2 30"]
+                [:td "Total Score i samtilige spil:"]
+                [:td "Player 1: " ((@tilstand :player1) :antalvundnespil) ", Player 2: " ((@tilstand :player2) :antalvundnespil)]
                 ]]]]]))
+
+
 
 ; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
